5b15ba8383637eaed04e272274effc114069ddf3
hashmat78692 <101383936+hashmat78692@users.noreply.github.com>
2024-11-09 09:34:32 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 sudo yum update -y &>/dev/null
 sudo yum install git -y &>/dev/null
 sudo yum install nodejs -y &>/dev/null
-git clone https://github.com/sheik-kajaalimohiddin/movie-search-react-app.git &>/dev/null
+git clone https://github.com/hashmat78692/introtocloudfinalproject.git &>/dev/null
 cd movie-search-react-app/
 npm install &>/dev/null
 npm start &>/dev/null
