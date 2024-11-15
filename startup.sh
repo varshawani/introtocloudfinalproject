ffa681c86a55ecceb0c7e14263807b8a3efeae08
hashmat78692 <101383936+hashmat78692@users.noreply.github.com>
2024-11-14 19:24:41 -0600

    Delete startup.sh

--- startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo yum update -y &>/dev/null
-sudo yum install git -y &>/dev/null
-sudo yum install nodejs -y &>/dev/null
-git clone https://github.com/hashmat78692/introtocloudfinalproject.git &>/dev/null
-cd movie-search-react-app/
-npm install &>/dev/null
-npm start &>/dev/null
